sigl/ps4-entrypoint-405

Modify loader to be fully relocatable.

loader.js
@@ -1,113 +1,74 @@
 function writeLoader(p, addr) {
-	p.write4(addr.add32(0x00000000), 0x00000be9);
-	p.write4(addr.add32(0x00000004), 0x0f2e6600);
-	p.write4(addr.add32(0x00000008), 0x0000841f);
-	p.write4(addr.add32(0x0000000C), 0x90000000);
-	p.write4(addr.add32(0x00000010), 0x54415541);
-	p.write4(addr.add32(0x00000014), 0x83485355);
-	p.write4(addr.add32(0x00000018), 0xd23118ec);
-	p.write4(addr.add32(0x0000001C), 0x000001be);
-	p.write4(addr.add32(0x00000020), 0x0002bf00);
-	p.write4(addr.add32(0x00000024), 0x04c60000);
-	p.write4(addr.add32(0x00000028), 0xb8481024);
-	p.write4(addr.add32(0x0000002C), 0x2610012f);
-	p.write4(addr.add32(0x00000030), 0x00000009);
-	p.write4(addr.add32(0x00000034), 0x012444c6);
-	p.write4(addr.add32(0x00000038), 0x08bc4902);
-	p.write4(addr.add32(0x0000003C), 0x09261001);
-	p.write4(addr.add32(0x00000040), 0xc7000000);
-	p.write4(addr.add32(0x00000044), 0x00042444);
-	p.write4(addr.add32(0x00000048), 0x66000000);
-	p.write4(addr.add32(0x0000004C), 0x022444c7);
-	p.write4(addr.add32(0x00000050), 0x44c63c23);
-	p.write4(addr.add32(0x00000054), 0xc6000a24);
-	p.write4(addr.add32(0x00000058), 0x000b2444);
-	p.write4(addr.add32(0x0000005C), 0x0c2444c6);
-	p.write4(addr.add32(0x00000060), 0x2444c600);
-	p.write4(addr.add32(0x00000064), 0x44c6000d);
-	p.write4(addr.add32(0x00000068), 0xc6000e24);
-	p.write4(addr.add32(0x0000006C), 0x000f2444);
-	p.write4(addr.add32(0x00000070), 0x10bad0ff);
-	p.write4(addr.add32(0x00000074), 0x48000000);
-	p.write4(addr.add32(0x00000078), 0x8941e689);
-	p.write4(addr.add32(0x0000007C), 0x48c789c5);
-	p.write4(addr.add32(0x00000080), 0x10013cb8);
-	p.write4(addr.add32(0x00000084), 0x00000926);
-	p.write4(addr.add32(0x00000088), 0xbed0ff00);
-	p.write4(addr.add32(0x0000008C), 0x0000000a);
-	p.write4(addr.add32(0x00000090), 0x48ef8944);
-	p.write4(addr.add32(0x00000094), 0x100149b8);
-	p.write4(addr.add32(0x00000098), 0x00000926);
-	p.write4(addr.add32(0x0000009C), 0x31d0ff00);
-	p.write4(addr.add32(0x000000A0), 0x44f631d2);
-	p.write4(addr.add32(0x000000A4), 0xb848ef89);
-	p.write4(addr.add32(0x000000A8), 0x26100122);
-	p.write4(addr.add32(0x000000AC), 0x00000009);
-	p.write4(addr.add32(0x000000B0), 0xc589d0ff);
-	p.write4(addr.add32(0x000000B4), 0x0000b848);
-	p.write4(addr.add32(0x000000B8), 0x00092620);
-	p.write4(addr.add32(0x000000BC), 0x00c60000);
-	p.write4(addr.add32(0x000000C0), 0xc38948c3);
-	p.write4(addr.add32(0x000000C4), 0x906607eb);
-	p.write4(addr.add32(0x000000C8), 0x01489848);
-	p.write4(addr.add32(0x000000CC), 0x1000bac3);
-	p.write4(addr.add32(0x000000D0), 0x89480000);
-	p.write4(addr.add32(0x000000D4), 0x41ef89de);
-	p.write4(addr.add32(0x000000D8), 0xc085d4ff);
-	p.write4(addr.add32(0x000000DC), 0x8944ea7f);
-	p.write4(addr.add32(0x000000E0), 0x15bb48ef);
-	p.write4(addr.add32(0x000000E4), 0x09261001);
-	p.write4(addr.add32(0x000000E8), 0xff000000);
-	p.write4(addr.add32(0x000000EC), 0xffef89d3);
-	p.write4(addr.add32(0x000000F0), 0x00b848d3);
-	p.write4(addr.add32(0x000000F4), 0x09262000);
-	p.write4(addr.add32(0x000000F8), 0xff000000);
-	p.write4(addr.add32(0x000000FC), 0xc48348d0);
-	p.write4(addr.add32(0x00000100), 0x415d5b18);
-	p.write4(addr.add32(0x00000104), 0xc35d415c);
-	p.write4(addr.add32(0x00000108), 0x03c0c748);
-	p.write4(addr.add32(0x0000010C), 0x49000000);
-	p.write4(addr.add32(0x00000110), 0x050fca89);
-	p.write4(addr.add32(0x00000114), 0xc0c748c3);
-	p.write4(addr.add32(0x00000118), 0x00000006);
-	p.write4(addr.add32(0x0000011C), 0x0fca8949);
-	p.write4(addr.add32(0x00000120), 0xc748c305);
-	p.write4(addr.add32(0x00000124), 0x00001ec0);
-	p.write4(addr.add32(0x00000128), 0xca894900);
-	p.write4(addr.add32(0x0000012C), 0x48c3050f);
-	p.write4(addr.add32(0x00000130), 0x0061c0c7);
-	p.write4(addr.add32(0x00000134), 0x89490000);
-	p.write4(addr.add32(0x00000138), 0xc3050fca);
-	p.write4(addr.add32(0x0000013C), 0x68c0c748);
-	p.write4(addr.add32(0x00000140), 0x49000000);
-	p.write4(addr.add32(0x00000144), 0x050fca89);
-	p.write4(addr.add32(0x00000148), 0xc0c748c3);
-	p.write4(addr.add32(0x0000014C), 0x0000006a);
-	p.write4(addr.add32(0x00000150), 0x0fca8949);
-	p.write4(addr.add32(0x00000154), 0x0000c305);
-	p.write4(addr.add32(0x00000158), 0x00000014);
-	p.write4(addr.add32(0x0000015C), 0x00000000);
-	p.write4(addr.add32(0x00000160), 0x00527a01);
-	p.write4(addr.add32(0x00000164), 0x01107801);
-	p.write4(addr.add32(0x00000168), 0x08070c1b);
-	p.write4(addr.add32(0x0000016C), 0x00000190);
-	p.write4(addr.add32(0x00000170), 0x00000034);
-	p.write4(addr.add32(0x00000174), 0x0000001c);
-	p.write4(addr.add32(0x00000178), 0xfffffe98);
-	p.write4(addr.add32(0x0000017C), 0x000000f8);
-	p.write4(addr.add32(0x00000180), 0x100e4200);
-	p.write4(addr.add32(0x00000184), 0x0e42028d);
-	p.write4(addr.add32(0x00000188), 0x41038c18);
-	p.write4(addr.add32(0x0000018C), 0x0486200e);
-	p.write4(addr.add32(0x00000190), 0x83280e41);
-	p.write4(addr.add32(0x00000194), 0x400e4405);
-	p.write4(addr.add32(0x00000198), 0x280ee702);
-	p.write4(addr.add32(0x0000019C), 0x41200e41);
-	p.write4(addr.add32(0x000001A0), 0x0e42180e);
-	p.write4(addr.add32(0x000001A4), 0x080e4210);
-	p.write4(addr.add32(0x000001A8), 0x3b031b01);
-	p.write4(addr.add32(0x000001AC), 0xffffffac);
-	p.write4(addr.add32(0x000001B0), 0x00000001);
-	p.write4(addr.add32(0x000001B4), 0xfffffe68);
-	p.write4(addr.add32(0x000001B8), 0xffffffc8);
+	p.write4(addr.add32(0x00000000), 0x54415541);
+	p.write4(addr.add32(0x00000004), 0x83485355);
+	p.write4(addr.add32(0x00000008), 0xd23118ec);
+	p.write4(addr.add32(0x0000000c), 0x000001be);
+	p.write4(addr.add32(0x00000010), 0x0002bf00);
+	p.write4(addr.add32(0x00000014), 0x04c60000);
+	p.write4(addr.add32(0x00000018), 0x44c61024);
+	p.write4(addr.add32(0x0000001c), 0xc7020124);
+	p.write4(addr.add32(0x00000020), 0x00042444);
+	p.write4(addr.add32(0x00000024), 0x66000000);
+	p.write4(addr.add32(0x00000028), 0x022444c7);
+	p.write4(addr.add32(0x0000002c), 0x44c63c23);
+	p.write4(addr.add32(0x00000030), 0xc6000a24);
+	p.write4(addr.add32(0x00000034), 0x000b2444);
+	p.write4(addr.add32(0x00000038), 0x0c2444c6);
+	p.write4(addr.add32(0x0000003c), 0x2444c600);
+	p.write4(addr.add32(0x00000040), 0x44c6000d);
+	p.write4(addr.add32(0x00000044), 0xc6000e24);
+	p.write4(addr.add32(0x00000048), 0x000f2444);
+	p.write4(addr.add32(0x0000004c), 0x00a115ff);
+	p.write4(addr.add32(0x00000050), 0x10ba0000);
+	p.write4(addr.add32(0x00000054), 0x48000000);
+	p.write4(addr.add32(0x00000058), 0x8941e689);
+	p.write4(addr.add32(0x0000005c), 0xffc789c5);
+	p.write4(addr.add32(0x00000060), 0x00009b15);
+	p.write4(addr.add32(0x00000064), 0x000abe00);
+	p.write4(addr.add32(0x00000068), 0x89440000);
+	p.write4(addr.add32(0x0000006c), 0x9a15ffef);
+	p.write4(addr.add32(0x00000070), 0x31000000);
+	p.write4(addr.add32(0x00000074), 0x44f631d2);
+	p.write4(addr.add32(0x00000078), 0x15ffef89);
+	p.write4(addr.add32(0x0000007c), 0x00000066);
+	p.write4(addr.add32(0x00000080), 0x8b48c589);
+	p.write4(addr.add32(0x00000084), 0x0fff7705);
+	p.write4(addr.add32(0x00000088), 0xc300c600);
+	p.write4(addr.add32(0x0000008c), 0xebc38948);
+	p.write4(addr.add32(0x00000090), 0x48984805);
+	p.write4(addr.add32(0x00000094), 0x00bac301);
+	p.write4(addr.add32(0x00000098), 0x48000010);
+	p.write4(addr.add32(0x0000009c), 0xef89de89);
+	p.write4(addr.add32(0x000000a0), 0x002615ff);
+	p.write4(addr.add32(0x000000a4), 0xc0850000);
+	p.write4(addr.add32(0x000000a8), 0x8944e77f);
+	p.write4(addr.add32(0x000000ac), 0x2615ffef);
+	p.write4(addr.add32(0x000000b0), 0x89000000);
+	p.write4(addr.add32(0x000000b4), 0x1e15ffef);
+	p.write4(addr.add32(0x000000b8), 0xff000000);
+	p.write4(addr.add32(0x000000bc), 0x0fff3f15);
+	p.write4(addr.add32(0x000000c0), 0xc4834800);
+	p.write4(addr.add32(0x000000c4), 0x415d5b18);
+	p.write4(addr.add32(0x000000c8), 0xc35d415c);
+	p.write4(addr.add32(0x000000cc), 0x03c0c748);
+	p.write4(addr.add32(0x000000d0), 0x49000000);
+	p.write4(addr.add32(0x000000d4), 0x050fca89);
+	p.write4(addr.add32(0x000000d8), 0xc0c748c3);
+	p.write4(addr.add32(0x000000dc), 0x00000006);
+	p.write4(addr.add32(0x000000e0), 0x0fca8949);
+	p.write4(addr.add32(0x000000e4), 0xc748c305);
+	p.write4(addr.add32(0x000000e8), 0x00001ec0);
+	p.write4(addr.add32(0x000000ec), 0xca894900);
+	p.write4(addr.add32(0x000000f0), 0x48c3050f);
+	p.write4(addr.add32(0x000000f4), 0x0061c0c7);
+	p.write4(addr.add32(0x000000f8), 0x89490000);
+	p.write4(addr.add32(0x000000fc), 0xc3050fca);
+	p.write4(addr.add32(0x00000100), 0x68c0c748);
+	p.write4(addr.add32(0x00000104), 0x49000000);
+	p.write4(addr.add32(0x00000108), 0x050fca89);
+	p.write4(addr.add32(0x0000010c), 0xc0c748c3);
+	p.write4(addr.add32(0x00000110), 0x0000006a);
+	p.write4(addr.add32(0x00000114), 0x0fca8949);
+	p.write4(addr.add32(0x00000118), 0x9090c305);
+	p.write4(addr.add32(0x0000011c), 0x90909090);
 }

vvsx87/ps4-hen-vtx

loader.js
@@ -1,113 +1,70 @@
 function writeLoader(p, addr) {
-	p.write4(addr.add32(0x00000000), 0x00000be9);
-	p.write4(addr.add32(0x00000004), 0x0f2e6600);
-	p.write4(addr.add32(0x00000008), 0x0000841f);
-	p.write4(addr.add32(0x0000000C), 0x90000000);
-	p.write4(addr.add32(0x00000010), 0x54415541);
-	p.write4(addr.add32(0x00000014), 0x83485355);
-	p.write4(addr.add32(0x00000018), 0xd23118ec);
-	p.write4(addr.add32(0x0000001C), 0x000001be);
-	p.write4(addr.add32(0x00000020), 0x0002bf00);
-	p.write4(addr.add32(0x00000024), 0x04c60000);
-	p.write4(addr.add32(0x00000028), 0xb8481024);
-	p.write4(addr.add32(0x0000002C), 0x2610012f);
-	p.write4(addr.add32(0x00000030), 0x00000009);
-	p.write4(addr.add32(0x00000034), 0x012444c6);
-	p.write4(addr.add32(0x00000038), 0x08bc4902);
-	p.write4(addr.add32(0x0000003C), 0x09261001);
-	p.write4(addr.add32(0x00000040), 0xc7000000);
-	p.write4(addr.add32(0x00000044), 0x00042444);
-	p.write4(addr.add32(0x00000048), 0x66000000);
-	p.write4(addr.add32(0x0000004C), 0x022444c7);
-	p.write4(addr.add32(0x00000050), 0x44c63c23);
-	p.write4(addr.add32(0x00000054), 0xc6000a24);
-	p.write4(addr.add32(0x00000058), 0x000b2444);
-	p.write4(addr.add32(0x0000005C), 0x0c2444c6);
-	p.write4(addr.add32(0x00000060), 0x2444c600);
-	p.write4(addr.add32(0x00000064), 0x44c6000d);
-	p.write4(addr.add32(0x00000068), 0xc6000e24);
-	p.write4(addr.add32(0x0000006C), 0x000f2444);
-	p.write4(addr.add32(0x00000070), 0x10bad0ff);
-	p.write4(addr.add32(0x00000074), 0x48000000);
-	p.write4(addr.add32(0x00000078), 0x8941e689);
-	p.write4(addr.add32(0x0000007C), 0x48c789c5);
-	p.write4(addr.add32(0x00000080), 0x10013cb8);
-	p.write4(addr.add32(0x00000084), 0x00000926);
-	p.write4(addr.add32(0x00000088), 0xbed0ff00);
-	p.write4(addr.add32(0x0000008C), 0x0000000a);
-	p.write4(addr.add32(0x00000090), 0x48ef8944);
-	p.write4(addr.add32(0x00000094), 0x100149b8);
-	p.write4(addr.add32(0x00000098), 0x00000926);
-	p.write4(addr.add32(0x0000009C), 0x31d0ff00);
-	p.write4(addr.add32(0x000000A0), 0x44f631d2);
-	p.write4(addr.add32(0x000000A4), 0xb848ef89);
-	p.write4(addr.add32(0x000000A8), 0x26100122);
-	p.write4(addr.add32(0x000000AC), 0x00000009);
-	p.write4(addr.add32(0x000000B0), 0xc589d0ff);
-	p.write4(addr.add32(0x000000B4), 0x0000b848);
-	p.write4(addr.add32(0x000000B8), 0x00092620);
-	p.write4(addr.add32(0x000000BC), 0x00c60000);
-	p.write4(addr.add32(0x000000C0), 0xc38948c3);
-	p.write4(addr.add32(0x000000C4), 0x906607eb);
-	p.write4(addr.add32(0x000000C8), 0x01489848);
-	p.write4(addr.add32(0x000000CC), 0x1000bac3);
-	p.write4(addr.add32(0x000000D0), 0x89480000);
-	p.write4(addr.add32(0x000000D4), 0x41ef89de);
-	p.write4(addr.add32(0x000000D8), 0xc085d4ff);
-	p.write4(addr.add32(0x000000DC), 0x8944ea7f);
-	p.write4(addr.add32(0x000000E0), 0x15bb48ef);
-	p.write4(addr.add32(0x000000E4), 0x09261001);
-	p.write4(addr.add32(0x000000E8), 0xff000000);
-	p.write4(addr.add32(0x000000EC), 0xffef89d3);
-	p.write4(addr.add32(0x000000F0), 0x00b848d3);
-	p.write4(addr.add32(0x000000F4), 0x09262000);
-	p.write4(addr.add32(0x000000F8), 0xff000000);
-	p.write4(addr.add32(0x000000FC), 0xc48348d0);
-	p.write4(addr.add32(0x00000100), 0x415d5b18);
-	p.write4(addr.add32(0x00000104), 0xc35d415c);
-	p.write4(addr.add32(0x00000108), 0x03c0c748);
-	p.write4(addr.add32(0x0000010C), 0x49000000);
-	p.write4(addr.add32(0x00000110), 0x050fca89);
-	p.write4(addr.add32(0x00000114), 0xc0c748c3);
-	p.write4(addr.add32(0x00000118), 0x00000006);
-	p.write4(addr.add32(0x0000011C), 0x0fca8949);
-	p.write4(addr.add32(0x00000120), 0xc748c305);
-	p.write4(addr.add32(0x00000124), 0x00001ec0);
-	p.write4(addr.add32(0x00000128), 0xca894900);
-	p.write4(addr.add32(0x0000012C), 0x48c3050f);
-	p.write4(addr.add32(0x00000130), 0x0061c0c7);
-	p.write4(addr.add32(0x00000134), 0x89490000);
-	p.write4(addr.add32(0x00000138), 0xc3050fca);
-	p.write4(addr.add32(0x0000013C), 0x68c0c748);
-	p.write4(addr.add32(0x00000140), 0x49000000);
-	p.write4(addr.add32(0x00000144), 0x050fca89);
-	p.write4(addr.add32(0x00000148), 0xc0c748c3);
-	p.write4(addr.add32(0x0000014C), 0x0000006a);
-	p.write4(addr.add32(0x00000150), 0x0fca8949);
-	p.write4(addr.add32(0x00000154), 0x0000c305);
-	p.write4(addr.add32(0x00000158), 0x00000014);
-	p.write4(addr.add32(0x0000015C), 0x00000000);
-	p.write4(addr.add32(0x00000160), 0x00527a01);
-	p.write4(addr.add32(0x00000164), 0x01107801);
-	p.write4(addr.add32(0x00000168), 0x08070c1b);
-	p.write4(addr.add32(0x0000016C), 0x00000190);
-	p.write4(addr.add32(0x00000170), 0x00000034);
-	p.write4(addr.add32(0x00000174), 0x0000001c);
-	p.write4(addr.add32(0x00000178), 0xfffffe98);
-	p.write4(addr.add32(0x0000017C), 0x000000f8);
-	p.write4(addr.add32(0x00000180), 0x100e4200);
-	p.write4(addr.add32(0x00000184), 0x0e42028d);
-	p.write4(addr.add32(0x00000188), 0x41038c18);
-	p.write4(addr.add32(0x0000018C), 0x0486200e);
-	p.write4(addr.add32(0x00000190), 0x83280e41);
-	p.write4(addr.add32(0x00000194), 0x400e4405);
-	p.write4(addr.add32(0x00000198), 0x280ee702);
-	p.write4(addr.add32(0x0000019C), 0x41200e41);
-	p.write4(addr.add32(0x000001A0), 0x0e42180e);
-	p.write4(addr.add32(0x000001A4), 0x080e4210);
-	p.write4(addr.add32(0x000001A8), 0x3b031b01);
-	p.write4(addr.add32(0x000001AC), 0xffffffac);
-	p.write4(addr.add32(0x000001B0), 0x00000001);
-	p.write4(addr.add32(0x000001B4), 0xfffffe68);
-	p.write4(addr.add32(0x000001B8), 0xffffffc8);
+	p.write4(addr.add32(0x00000000), 0x54415541);
+	p.write4(addr.add32(0x00000004), 0x83485355);
+	p.write4(addr.add32(0x00000008), 0xd23118ec);
+	p.write4(addr.add32(0x0000000c), 0x000001be);
+	p.write4(addr.add32(0x00000010), 0x0002bf00);
+	p.write4(addr.add32(0x00000014), 0x04c60000);
+	p.write4(addr.add32(0x00000018), 0x44c61024);
+	p.write4(addr.add32(0x0000001c), 0xc7020124);
+	p.write4(addr.add32(0x00000020), 0x00042444);
+	p.write4(addr.add32(0x00000024), 0x66000000);
+	p.write4(addr.add32(0x00000028), 0x022444c7);
+	p.write4(addr.add32(0x0000002c), 0x44c63c23);
+	p.write4(addr.add32(0x00000030), 0xc6000a24);
+	p.write4(addr.add32(0x00000034), 0x000b2444);
+	p.write4(addr.add32(0x00000038), 0x0c2444c6);
+	p.write4(addr.add32(0x0000003c), 0x2444c600);
+	p.write4(addr.add32(0x00000040), 0x44c6000d);
+	p.write4(addr.add32(0x00000044), 0xc6000e24);
+	p.write4(addr.add32(0x00000048), 0x000f2444);
+	p.write4(addr.add32(0x0000004c), 0x000097e8);
+	p.write4(addr.add32(0x00000050), 0x0010ba00);
+	p.write4(addr.add32(0x00000054), 0x89480000);
+	p.write4(addr.add32(0x00000058), 0xc58941e6);
+	p.write4(addr.add32(0x0000005c), 0x92e8c789);
+	p.write4(addr.add32(0x00000060), 0xbe000000);
+	p.write4(addr.add32(0x00000064), 0x0000000a);
+	p.write4(addr.add32(0x00000068), 0xe8ef8944);
+	p.write4(addr.add32(0x0000006c), 0x00000092);
+	p.write4(addr.add32(0x00000070), 0xf631d231);
+	p.write4(addr.add32(0x00000074), 0xe8ef8944);
+	p.write4(addr.add32(0x00000078), 0x0000005f);
+	p.write4(addr.add32(0x0000007c), 0x8d48c589);
+	p.write4(addr.add32(0x00000080), 0x0fff7b1d);
+	p.write4(addr.add32(0x00000084), 0xc303c600);
+	p.write4(addr.add32(0x00000088), 0x984805eb);
+	p.write4(addr.add32(0x0000008c), 0xbac30148);
+	p.write4(addr.add32(0x00000090), 0x00001000);
+	p.write4(addr.add32(0x00000094), 0x89de8948);
+	p.write4(addr.add32(0x00000098), 0x0023e8ef);
+	p.write4(addr.add32(0x0000009c), 0xc0850000);
+	p.write4(addr.add32(0x000000a0), 0x8944e87f);
+	p.write4(addr.add32(0x000000a4), 0x0024e8ef);
+	p.write4(addr.add32(0x000000a8), 0xef890000);
+	p.write4(addr.add32(0x000000ac), 0x00001de8);
+	p.write4(addr.add32(0x000000b0), 0xff4ae800);
+	p.write4(addr.add32(0x000000b4), 0x8348000f);
+	p.write4(addr.add32(0x000000b8), 0x5d5b18c4);
+	p.write4(addr.add32(0x000000bc), 0x5d415c41);
+	p.write4(addr.add32(0x000000c0), 0xc0c748c3);
+	p.write4(addr.add32(0x000000c4), 0x00000003);
+	p.write4(addr.add32(0x000000c8), 0x0fca8949);
+	p.write4(addr.add32(0x000000cc), 0xc748c305);
+	p.write4(addr.add32(0x000000d0), 0x000006c0);
+	p.write4(addr.add32(0x000000d4), 0xca894900);
+	p.write4(addr.add32(0x000000d8), 0x48c3050f);
+	p.write4(addr.add32(0x000000dc), 0x001ec0c7);
+	p.write4(addr.add32(0x000000e0), 0x89490000);
+	p.write4(addr.add32(0x000000e4), 0xc3050fca);
+	p.write4(addr.add32(0x000000e8), 0x61c0c748);
+	p.write4(addr.add32(0x000000ec), 0x49000000);
+	p.write4(addr.add32(0x000000f0), 0x050fca89);
+	p.write4(addr.add32(0x000000f4), 0xc0c748c3);
+	p.write4(addr.add32(0x000000f8), 0x00000068);
+	p.write4(addr.add32(0x000000fc), 0x0fca8949);
+	p.write4(addr.add32(0x00000100), 0xc748c305);
+	p.write4(addr.add32(0x00000104), 0x00006ac0);
+	p.write4(addr.add32(0x00000108), 0xca894900);
+	p.write4(addr.add32(0x0000010c), 0x90c3050f);
 }