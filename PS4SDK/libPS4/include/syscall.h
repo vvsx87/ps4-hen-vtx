9b16f18d621f06abebd9e0009d7e2c68d2e0070b
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-06-16 16:16:43 -0600

    Added SDK

---
+++ PS4SDK/libPS4/include/syscall.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef SYSCALL_H
+#define SYSCALL_H
+
+#include "types.h"
+
+#define SYSCALL(name, number)        \
+  __asm__(".intel_syntax noprefix"); \
+  __asm__(".globl " #name "");       \
+  __asm__("" #name ":");             \
+  __asm__("movq rax, " #number "");  \
+  __asm__("jmp syscall_macro");
+
+unsigned long syscall(unsigned long n, ...);
+
+#endif
