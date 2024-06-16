0f2b7062af22cd7ca5390e245d9714898467770e
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-06-16 16:41:32 -0600

    Added SDK

---
+++ PS4SDK/libPS4/include/eventflag.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef EVENTFLAG_H
+#define EVENTFLAG_H
+
+#include "types.h"
+
+int createEventFlag(const char *name);
+int destroyEventFlag(int eventFlag);
+
+#endif
---
+++ PS4SDK/libPS4/include/unknown.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef UNKNOWN_H
+#define UNKNOWN_H
+
+#include "types.h"
+
+int unknownResourceCreate(const char *name);
+int unknownResourceDestroy(int unknownResource);
+
+#endif
