0ff1d8d36715ad92173bc26213c6e66e2c99ad09
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-06-16 16:53:08 -0600

    Added SDK

---
+++ PS4SDK/libPS4/include/semaphore.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef SEMAPHORE_H
+#define SEMAPHORE_H
+
+#include "types.h"
+
+int createSemaphore(const char *name, int attributes, int startingCount, int maxCount);
+int removeSemaphore(int semaphore);
+int openSemaphore(const char *name, int oflag, int mode, unsigned int value);
+int closeSemaphore(int semaphore);
+int waitSemaphore(int semaphore, int requiredCount, int *microsecondTimeout);
+int pollSemaphore(int semaphore, int requiredCount);
+int signalSemaphore(int semaphore, int count);
+int cancelSemaphore(int semaphore, int count, int *threadsReleased);
+
+#endif
