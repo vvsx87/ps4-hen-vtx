09afd07bd949cbac7b29b389caa8ae10397b7c7a
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-06-18 21:01:11 -0600

    Added 852 Files

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ pushd installer
 make
 popd
 
-rm -f ps4-hen-vtx.bin
-cp installer/installer.bin ps4-hen-vtx.bin
+rm -f ps4-hen-852-PPPwn-vtx.bin
+cp installer/installer.bin ps4-hen-852-PPPwn-vtx.bin
