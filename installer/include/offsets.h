0ad6ae08da3b515994c1d274aa98fc2596f6acb6
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-06-23 08:10:17 -0600

    Updated To 2.1.4

--- installer/include/offsets.h
+++ installer/include/offsets.h
@@ -2,7 +2,7 @@
 #define __OFFSETS_H__
 #pragma once
 
-// 10.50
+// 10.70 - 10.71
 #define	XFAST_SYSCALL_addr              0x000001C0
 
 // Names - Data
@@ -10,13 +10,10 @@
 #define ROOTVNODE_addr                  0x01BF81F0
 #define PMAP_STORE_addr                 0x01B2CEE0
 #define DT_HASH_SEGMENT_addr            0x00CE7008
-#define M_TEMP_addr                     0x01A5FE30
 
 // Functions
 #define pmap_protect_addr               0x00046EF0
 #define pmap_protect_p_addr             0x00046F37
-#define malloc_addr                     0x0036E120
-#define printf_addr                     0x00450E80
 
 // Patches
 // debug menu error
@@ -34,13 +31,10 @@
 #define sys_dynlib_dlsym_patch1         0x00213088
 #define sys_dynlib_dlsym_patch2         0x002DAB60
 
-// sdk version spoof - enable all VR fws
-//#define sdk_version_patch               0x01A6D810
-
 // enable debug log
-//#define enable_debug_log_patch          0x00450F67
+#define enable_debug_log_patch          0x00450F67
 
 // enable uart output
-//#define enable_uart_patch               0x0
+#define enable_uart_patch               0x01A3BCA0
 
 #endif
