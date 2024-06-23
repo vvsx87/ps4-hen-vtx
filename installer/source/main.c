fcc2eb10634ff55529886487130568366bd971a1
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-06-23 11:39:17 -0600

    Added 9.51 files

---
+++ installer/source/main.c
@@ -0,0 +1,153 @@
+#include <ps4.h>
+
+#include "defines.h"
+#include "ddebug.h"
+#include "offsets.h"
+
+#define PS4_UPDATE_FULL_PATH "/update/PS4UPDATE.PUP"
+#define PS4_UPDATE_TEMP_PATH "/update/PS4UPDATE.PUP.net.temp"
+
+extern char kpayload[];
+extern unsigned kpayload_size;
+
+int install_payload(struct thread *td, struct payload_info* payload_info)
+{
+
+	uint8_t* kernel_base = (uint8_t*)(__readmsr(0xC0000082) - XFAST_SYSCALL_addr);
+
+	void (*pmap_protect)(void * pmap, uint64_t sva, uint64_t eva, uint8_t pr) = (void *)(kernel_base + pmap_protect_addr);
+	void *kernel_pmap_store = (void *)(kernel_base + PMAP_STORE_addr);
+
+	uint8_t* payload_data = payload_info->buffer;
+	size_t payload_size = payload_info->size;
+	struct payload_header* payload_header = (struct payload_header*)payload_data;
+	uint8_t* payload_buffer = (uint8_t*)&kernel_base[DT_HASH_SEGMENT_addr];
+
+	if (!payload_data || payload_size < sizeof(payload_header) || payload_header->signature != 0x5041594C4F414458ull)
+		return -1;
+
+	// Use "kmem" for all patches
+        uint8_t *kmem;
+
+	// Disable write protection
+	uint64_t cr0 = readCr0();
+	writeCr0(cr0 & ~X86_CR0_WP);
+
+	// Patch debug setting errors
+	kmem = (uint8_t *)&kernel_base[debug_menu_error_patch1];
+	kmem[0] = 0x00;
+	kmem[1] = 0x00;
+	kmem[2] = 0x00;
+	kmem[3] = 0x00;
+
+	kmem = (uint8_t *)&kernel_base[debug_menu_error_patch2];
+	kmem[0] = 0x00;
+	kmem[1] = 0x00;
+	kmem[2] = 0x00;
+	kmem[3] = 0x00;
+
+	// flatz disable pfs signature check
+	kmem = (uint8_t *)&kernel_base[disable_signature_check_patch];
+	kmem[0] = 0x31;
+	kmem[1] = 0xC0;
+	kmem[2] = 0xC3;
+
+	// flatz enable debug RIFs
+	kmem = (uint8_t *)&kernel_base[enable_debug_rifs_patch1];
+	kmem[0] = 0xB0;
+	kmem[1] = 0x01;
+	kmem[2] = 0xC3;
+
+	kmem = (uint8_t *)&kernel_base[enable_debug_rifs_patch2];
+	kmem[0] = 0xB0;
+	kmem[1] = 0x01;
+	kmem[2] = 0xC3;
+
+        //flatz Patch sys_dynlib_dlsym: Allow from anywhere
+	kmem = (uint8_t *)&kernel_base[sys_dynlib_dlsym_patch1];
+	kmem[0] = 0xEB;
+	kmem[1] = 0x4C;
+
+	kmem = (uint8_t *)&kernel_base[sys_dynlib_dlsym_patch2];
+	kmem[0] = 0x31;
+	kmem[1] = 0xC0;
+	kmem[2] = 0xC3;
+
+	// Enable *all* debugging logs (in vprintf)
+	// Patch by: SiSTRo
+	kmem = (uint8_t *)&kernel_base[enable_debug_log_patch];
+	kmem[0] = 0xEB;
+	kmem[1] = 0x3B;
+
+	// Enable UART
+	kmem = (uint8_t *)&kernel_base[enable_uart_patch];
+	kmem[0] = 0x00;
+	kmem[1] = 0x00;
+	kmem[2] = 0x00;
+	kmem[3] = 0x00;
+
+	// install kpayload
+	memset(payload_buffer, 0, PAGE_SIZE);
+	memcpy(payload_buffer, payload_data, payload_size);
+
+	uint64_t sss = ((uint64_t)payload_buffer) & ~(uint64_t)(PAGE_SIZE-1);
+	uint64_t eee = ((uint64_t)payload_buffer + payload_size + PAGE_SIZE - 1) & ~(uint64_t)(PAGE_SIZE-1);
+	kernel_base[pmap_protect_p_addr] = 0xEB;
+	pmap_protect(kernel_pmap_store, sss, eee, 7);
+	kernel_base[pmap_protect_p_addr] = 0x75;
+
+	// Restore write protection
+	writeCr0(cr0);
+
+	int (*payload_entrypoint)();
+	*((void**)&payload_entrypoint) = (void*)(&payload_buffer[payload_header->entrypoint_offset]);
+
+	return payload_entrypoint();
+}
+
+static inline void patch_update(void)
+{
+	unlink(PS4_UPDATE_FULL_PATH);
+	unlink(PS4_UPDATE_TEMP_PATH);
+
+	mkdir(PS4_UPDATE_FULL_PATH, 0777);
+	mkdir(PS4_UPDATE_TEMP_PATH, 0777);
+}
+
+int _main(struct thread *td) 
+{
+	int result;
+
+	initKernel();
+	initLibc();
+
+#ifdef DEBUG_SOCKET
+	initNetwork();
+	initDebugSocket();
+#endif
+
+	printfsocket("Starting...\n");
+
+	struct payload_info payload_info;
+	payload_info.buffer = (uint8_t *)kpayload;
+	payload_info.size = (size_t)kpayload_size;
+
+	errno = 0;
+
+	result = kexec(&install_payload, &payload_info);
+	result = !result ? 0 : errno;
+	printfsocket("install_payload: %d\n", result);
+
+	patch_update();
+
+	initSysUtil();
+	notify("Welcome to PS4HEN v"VERSION);
+
+	printfsocket("Done.\n");
+
+#ifdef DEBUG_SOCKET
+	closeDebugSocket();
+#endif
+
+	return result;
+}
